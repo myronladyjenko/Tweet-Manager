fa7201f29dcccbae63d23c8b4b11873fc6858777
Myron Ladyjenko <mladyjen@fred.socs.uoguelph.ca>
2022-03-20 15:44:04 -0400

    First commit

---
+++ mainA3.c
@@ -0,0 +1,92 @@
+#include "headerA3.h"
+#include "headerHelperA3.h"
+
+int main()
+{
+    int checkCurr = 0;
+    char choiceStr[101]; 
+    int choice = 0; 
+
+    do {
+            printf ("\n1. Create a new tweet\n");
+            printf ("2. Display tweets\n");
+            printf ("3. Search a tweet (by keyword)\n");
+            printf ("4. Find how many words are “stop words” (stop words are explained in a later section)\n");
+            printf ("5. Delete the nth tweet\n");
+            printf ("6. Save tweets to a file\n");
+            printf ("7. Load tweets from a file\n");
+            printf ("8. Sort the given linked list on userid\n");
+            printf ("9. Exit\n");
+            
+            // user input for the options provided in the menu (chekcs for invalid type input
+            while (checkCurr < 1) 
+            {
+               printf ("Enter your choice. Please choose from 1 to 9: ");
+               fgets(choiceStr, 100, stdin);
+             
+               for (int i = 0; choiceStr[i] != '\n'; i++)
+               {
+                  
+                  if ( isdigit(choiceStr[i]) ) 
+                  {  
+                    checkCurr = 1;
+                  }
+                  else
+                  {
+                     checkCurr = 0;
+                     break;
+                  }     
+               }
+            }
+
+            checkCurr = 0;
+            choice = atoi(choiceStr);
+            
+            // swithc statement for each of the options provided in the menu
+            switch (choice) 
+            {
+                case 1:
+                  printf("Calls: createTweet.c\n");
+                  break;
+
+                case 2:
+                    printf("Calls: displayTweets.c\n");
+                    break;
+
+                case 3:
+                    printf("Calls: searchTweetsByKeyword.c\n");
+                    break;
+
+                case 4:
+                    printf("Calls: countStopWords.c\n");
+                    break;
+
+                case 5:
+                    printf("Calls: deleteTweet.c\n");
+                    break;
+
+                case 6:
+                    printf("Calls: saveTweetsToFile.c\n");
+                    break;
+
+                case 7:
+                    printf("Calls: loadTweetsFromFile.c\n");
+                    break;
+
+                case 8:
+                    printf("Calls: sortID.c\n");
+                    break;
+
+                case 9:
+                    printf("Exiting...\n");
+                    break;
+
+                // validity check for the input for the choice variable 
+               default: printf ("That is an invalid choice. Please choose from 1 to 9\n");
+          
+            }
+      
+      checkCurr = 0;
+    } while (choice != 9);
+    
+}
---
+++ makefile
@@ -0,0 +1,38 @@
+CC=gcc
+CFLAGS=-Wall -std=c99
+
+ladyjenkoMyronA3: addNodeToList.o sortID.o loadTweetsFromFile.o saveTweetsToFile.o deleteTweet.o countStopWords.o searchTweetsByKeyword.o displayTweets.o createTweet.o mainA3.o
+	$(CC) $(CFLAGS) addNodeToList.o sortID.o loadTweetsFromFile.o saveTweetsToFile.o deleteTweet.o countStopWords.o searchTweetsByKeyword.o displayTweets.o createTweet.o mainA3.o -o ladyjenkoMyronA3
+
+addNodeToList.o: addNodeToList.c
+	$(CC) $(CFLAGS) -c addNodeToList.c
+
+sortID.o: sortID.c
+	$(CC) $(CFLAGS) -c sortID.c
+
+loadTweetsFromFile.o: loadTweetsFromFile.c
+	$(CC) $(CFLAGS) -c loadTweetsFromFile.c
+
+saveTweetsToFile.o: saveTweetsToFile.c
+	$(CC) $(CFLAGS) -c saveTweetsToFile.c
+
+deleteTweet.o: deleteTweet.c
+	$(CC) $(CFLAGS) -c deleteTweet.c	
+
+countStopWords.o: countStopWords.c
+	$(CC) $(CFLAGS) -c countStopWords.c
+
+searchTweetsByKeyword.o: searchTweetsByKeyword.c
+	$(CC) $(CFLAGS) -c searchTweetsByKeyword.c
+
+displayTweets.o: displayTweets.c
+	$(CC) $(CFLAGS) -c displayTweets.c
+
+createTweet.o: createTweet.c
+	$(CC) $(CFLAGS) -c createTweet.c
+
+mainA3.o: mainA3.c
+	$(CC) $(CFLAGS) -c mainA3.c
+	
+clean: 
+	rm *.o ladyjenkoMyronA3
